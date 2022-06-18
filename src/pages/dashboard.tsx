5faeee7d7a9e08cbab87f3dfde1eab88fa1668c3
viniciusFreitas00 <vini.freirefreitas00@gmail.com>
2022-06-18 20:03:20 -0300

    :lipstick: create sidebar

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,16 @@
-import { Header } from '../components/Header'
+import { Flex } from '@chakra-ui/react';
+
+import { Sidebar } from '../components/Sidebar';
+import { Header } from '../components/Header';
 
 export default function Dashboard() {
-  return(
-    <Header />
+  return (
+    <Flex direction="column" h="100vh">
+      <Header />
+
+      <Flex w="100%" my="6" maxW={1480} marginX="auto" px="6">
+        <Sidebar />
+      </Flex>
+    </Flex>
   )
 }
