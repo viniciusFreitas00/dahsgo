ed373b53b118d601a0a84e002d27cc1205b16643
viniciusFreitas00 <vini.freirefreitas00@gmail.com>
2022-06-22 22:24:24 -0300

    :lipstick: responsive header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,19 +1,25 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex
       align="center"
     >
-      <Box
-        mr="4"
-        textAlign="center"
-      >
-        <Text>Vinicius Freitas</Text>
-        <Text color="gray.300" fontSize="small" >
-          vini.freifrefreitas@gmail.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box
+          mr="4"
+          textAlign="center"
+        >
+          <Text>Vinicius Freitas</Text>
+          <Text color="gray.300" fontSize="small" >
+            vini.freifrefreitas@gmail.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar size="md" name="Vinicius Freitas" src="https://github.com/viniciusFreitas00.png" />
     </Flex>
