f479565fbff7bbe9922a18d4531ff7a76397fd36
viniciusFreitas00 <vini.freirefreitas00@gmail.com>
2022-06-25 21:53:44 -0300

    :sparkles: add navigation

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,8 +1,9 @@
 import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text, useBreakpointValue } from "@chakra-ui/react";
+import Link from "next/link";
 import { Header } from "components/Header";
 import { Pagination } from "components/Pagination";
 import { Sidebar } from "components/Sidebar";
-import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { RiAddLine } from "react-icons/ri";
 
 export default function UserList() {
   const isWideVersion = useBreakpointValue({
@@ -21,15 +22,17 @@ export default function UserList() {
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">Usuários</Heading>
 
-            <Button
-              as="a"
-              size="sm"
-              fontSize="sm"
-              colorScheme="pink"
-              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
-            >
-              Criar novo
-            </Button>
+            <Link href="/users/create" passHref>
+              <Button
+                as="a"
+                size="sm"
+                fontSize="sm"
+                colorScheme="pink"
+                leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+              >
+                Criar novo
+              </Button>
+            </Link>
           </Flex>
 
           <Table colorScheme="whiteAlpha">
